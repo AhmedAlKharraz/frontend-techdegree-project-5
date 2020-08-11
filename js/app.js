23a33004edc5886658dd6d2e6ee2b101b7469e60
AhmedAlKharraz <64503987+AhmedAlKharraz@users.noreply.github.com>
2020-08-11 20:03:29 +0400

    Edit search

--- js/app.js
+++ js/app.js
@@ -14,7 +14,8 @@ function myFunction() {
     // Loop through all img items, and hide those who don't match the search query
     for (i = 0; i < img.length; i++) {
         txtValue = img[i].getElementsByTagName("img")[0].alt;
-        if (txtValue.toLowerCase().indexOf(filter) > -1) {
+        txtValue2 = img[i].getElementsByTagName("a")[0].getAttribute("data-caption");;
+        if (txtValue.toLowerCase().indexOf(filter) > -1 || txtValue2.toLowerCase().indexOf(filter) > -1) {
             img[i].style.display = "";
         } else {
             img[i].style.display = "none";
